16f8c2cdfd7637580ffef4fbb2a009783cc8b25a
jonghakseo <unqocn@gmail.com>
2022-01-02 14:24:40 +0900

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -131,7 +131,7 @@ const IndexPage = () => {
     <main style={pageStyles}>
       <title>JongHakSeo Page</title>
       <h1 style={headingStyles}>
-        HI!
+        HI! test
         <br />
         <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
         <span role="img" aria-label="Party popper emojis">
