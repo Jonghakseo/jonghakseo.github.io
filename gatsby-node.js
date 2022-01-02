e3ae09e1f7674460d1e498fed3679725521fc5cb
jonghakseo <unqocn@gmail.com>
2022-01-02 19:11:13 +0900

    update

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,20 +4,13 @@ const { createFilePath } = require("gatsby-source-filesystem");
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions;
-
-  // 새로운 노드가 생성 됐을떄 실행 되는 함수
   if (node.internal.type === `MarkdownRemark`) {
-    // markdown 노드가 생성 될떄만 콘솔 찍음
-    const fileNode = getNode(node.parent);
     const slug = createFilePath({ node, getNode, basePath: "pages/post" });
     createNodeField({
       node,
       name: `slug`,
       value: slug,
     });
-    console.log(createFilePath({ node, getNode, basePath: `pages` }));
-    //파일명으로 정보를y 얻어옴
-    console.log(`\n`, fileNode.relativePath);
   }
 };
 
@@ -38,8 +31,6 @@ exports.createPages = async ({ graphql, actions }) => {
     }
   `);
 
-  console.log(JSON.stringify(result, null, 4));
-
   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
     createPage({
       path: `post${node.fields.slug}`,
@@ -53,7 +44,11 @@ exports.createPages = async ({ graphql, actions }) => {
 
 exports.onPreInit = () => {
   if (process.argv[2] === "build") {
-    fs.rmSync(path.join(__dirname, "docs"), { recursive: true });
+    try {
+      fs.rmSync(path.join(__dirname, "docs"), { recursive: true });
+    } catch (e) {
+      console.error(e);
+    }
   }
 };
 
