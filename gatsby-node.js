f53680640c39587e529b009612f0a72e5014becb
jonghakseo <unqocn@gmail.com>
2022-01-02 13:54:39 +0900

    init gatsby

---
+++ gatsby-node.js
@@ -0,0 +1,15 @@
+const path = require("path")
+const fs = require("fs")
+
+exports.onPreInit = () => {
+    if (process.argv[2] === "build") {
+        fs.rmdirSync(path.join(__dirname, "docs"), { recursive: true })
+    }
+}
+
+exports.onPostBuild = () => {
+    fs.renameSync(
+        path.join(__dirname, "public"),
+        path.join(__dirname, "docs")
+    )
+}
